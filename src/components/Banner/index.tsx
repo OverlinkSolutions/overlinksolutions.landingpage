0f88f963d309950c3eb09f25b464747114564b31
andavas <andesonalmeida8@gmail.com>
2023-03-02 11:04:05 -0300

    FEAT: Adicionando responsividade

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,16 +1,18 @@
 import banner from "./banner.module.sass";
-import bannerImg from "../../images/coworking.jpg";
+import bannerImg from "../../images/icon_formaDeConexao.png";
 
 export default function Banner() {
   return (
-    <section id={banner.container} className="container">
-      <img src={bannerImg} alt="" />
+    <section id={banner.container} className="container autowrap">
       <div id={banner.text} className="container column">
         <h1>Uma nova forma de conexão com seus clientes</h1>
         <div className="container column">
           <span>O nosso negócio</span>
           <span>é divulgar o seu.</span>
         </div>
+      </div>
+      <div id={banner.logo_container} className="container">
+        <img src={bannerImg} alt="Forma de conexão com seu negócio" />
       </div>
     </section>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,54 @@
 import { Button } from "antd";
 import * as React from "react";
-import logo from '../../images/logo3.png';
+import { useMediaQuery } from "react-responsive";
+import logo from "../../images/logo3.png";
 import header from "./header.module.sass";
 
 export default function Header() {
+  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const [isClickTriggeredByEffect, setIsClickTriggeredByEffect] = React.useState(false);
+  const buttonRef = React.useRef<HTMLButtonElement>(null);
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      if (buttonRef.current) {
+        setIsClickTriggeredByEffect(true);
+        buttonRef.current.click();
+      }
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const handleClick = () => {
+    if (isClickTriggeredByEffect) {
+      setIsClickTriggeredByEffect(false);
+      return;
+    }
+
+  };
+
   return (
     <header id={header.container} className="container row">
       <div id={header.logo_container} className="container">
         <img src={logo} alt="Logo" />
       </div>
-      <nav id={header.btn_row} className="container row">
-        <Button type="text">Quem somos</Button>
-        <Button type="text">Nosso trabalho</Button>
-        <Button type="primary">Contato</Button>
-      </nav>
+      {isMobile ? (
+        <nav id={header.btn_row} className="container row">
+          <Button type="primary" ref={buttonRef}>
+            Contato
+          </Button>
+        </nav>
+      ) : (
+        <nav id={header.btn_row} className="container row">
+          <Button type="text">Quem somos</Button>
+          <Button type="text">Nosso trabalho</Button>
+          <Button type="primary" ref={buttonRef}>
+            Contato
+          </Button>
+        </nav>
+      )}
     </header>
   );
 }
